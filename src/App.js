a332c5dedf5ef81679d4a8fe60c76311d09c0cf5
wambuguedison <wambuguedison@gmail.com>
2020-09-19 19:43:03 +0300

    render contact div

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
+import { Container, Col, Row } from "react-bootstrap";
 import Carousel from "./components/Carousel";
 import Info from "./components/Info";
+import Contact from "./components/Contact";
 import "./App.css";
 
 function App() {
@@ -8,7 +10,16 @@ function App() {
     <div className="App">
       <h2>Bravo Leather</h2>
       <Carousel />
-      <Info />
+      <Container fluid>
+        <Row>
+          <Col md={6}>
+            <Info />
+          </Col>
+          <Col md={6}>
+            <Contact />
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
