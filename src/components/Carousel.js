c469e7c4fbda68cd7c16e286472e179bdc8d69a1
wambuguedison <wambuguedison@gmail.com>
2020-09-19 16:34:15 +0300

    Import Owl carousel dependancies

---
+++ src/components/Carousel.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import OwlCarousel from 'react-owl-carousel';
+import 'owl.carousel/dist/assets/owl.carousel.css';
+import 'owl.carousel/dist/assets/owl.theme.default.css'
+import './Carousel.css';
