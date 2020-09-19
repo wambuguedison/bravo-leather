6d7e43bfcbcd300c1a3fd392ee229dd124859f10
wambuguedison <wambuguedison@gmail.com>
2020-09-19 19:42:25 +0300

    Add contact icons

---
+++ src/components/Contact.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { Jumbotron, Container } from "react-bootstrap";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import "./Info.css";
+
+library.add(fas, fab);
+
+class Info extends React.Component {
+  render() {
+    return (
+      <div>
+        <Jumbotron fluid>
+          <Container>
+            <h4>Get in touch with us</h4>
+            <div>
+              <FontAwesomeIcon className="phone m-1" icon={["fas", "phone"]} />
+              <FontAwesomeIcon
+                className="whatsapp m-1"
+                icon={["fab", "whatsapp"]}
+              />
+              <FontAwesomeIcon
+                className="facebook m-1"
+                icon={["fab", "facebook"]}
+              />
+            </div>
+          </Container>
+        </Jumbotron>
+      </div>
+    );
+  }
+}
+
+export default Info;
