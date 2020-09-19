f8eb9a314892366ceca1e67374663acc66b0908e
wambuguedison <wambuguedison@gmail.com>
2020-09-19 18:12:01 +0300

    Call to attention

---
+++ src/components/Info.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Jumbotron, Container } from "react-bootstrap";
+import "./Info.css";
+
+class Info extends React.Component {
+  render() {
+    return (
+      <div>
+        <Jumbotron fluid>
+          <Container>
+            <h3>Welcome to Bravo leather</h3>
+            <p>
+              Bravo Leather is a well established leather dealer that has you
+              covered when it comes to leather products.
+            </p>
+          </Container>
+        </Jumbotron>
+      </div>
+    );
+  }
+}
+
+export default Info;
