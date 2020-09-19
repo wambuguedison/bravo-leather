0a24a3c14fc46977bf6ba6704f22cc826975f46d
wambuguedison <wambuguedison@gmail.com>
2020-09-19 17:34:42 +0300

    Render carousel

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import Carousel from "./components/Carousel";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <h2>Bravo Leather</h2>
+      <Carousel />
     </div>
   );
 }
